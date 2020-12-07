70d35b7bc5b9324fc5a4a94453789b7a00d24e26
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2020-12-06 17:17:04 -0800

    clear up comments

--- src/components/summary-sections.js
+++ src/components/summary-sections.js
@@ -1,20 +1,7 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
 import styled from "styled-components";
-import BackgroundImage from "gatsby-background-image";
 import Img from "gatsby-image";
-
-const StyledInnerWrapper = styled.div`
-  min-height: 500px;
-  width: 60%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: auto;
-  div {
-    margin: auto;
-  }
-`;
 
 const StyledImageWrapper = styled.div`
   min-height: 500px;
@@ -44,9 +31,6 @@ const Summaries = ({ currentUnit }) => {
   );
 
   return (
-    // <BackgroundImage fluid={data["unit" + currentUnit].childImageSharp.fluid}>
-    //   <StyledInnerWrapper></StyledInnerWrapper>
-    // </BackgroundImage>
     <StyledImageWrapper>
       <Img fluid={data["unit" + currentUnit].childImageSharp.fluid} />
     </StyledImageWrapper>
